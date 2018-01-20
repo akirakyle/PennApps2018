70297ea310b76f9acb988094943ac4fea8ffe21b
akirakyle <akyle@cmu.edu>
2018-01-20 13:32:25 -0500

    spotify oauth2 now works!

--- home/views.py
+++ home/views.py
@@ -3,7 +3,17 @@ from django.http import HttpResponse
 from home.spotify import Spotify
 
 # Create your views here.
+spot = Spotify()
 
 def index(request):
-    Spotify()
-    return HttpResponse("Hello, world. You're at the polls index.")
+    auth_url = spot.get_auth_url()
+    if auth_url:
+        htmlLoginButton = "<a href='" + auth_url + "'>Login to Spotify</a>"
+    else:
+        htmlLoginButton = "you're already logged into spotify"
+    return HttpResponse(htmlLoginButton)
+
+def auth_page(request):
+    print(request.get_full_path())
+    spot.auth(request.get_full_path())
+    return HttpResponse("should be logged in")
