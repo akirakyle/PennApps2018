b14c4d7f4037fe554a4b82f7fb8a774127fd2fa2
akirakyle <akyle@cmu.edu>
2018-01-20 23:21:50 -0500

    remove unused artists app and .sass-cache and __pycache__

--- artists/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-            path('<int:artist_id>', views.detail, name='detail'),
-            ]
--- artists/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import get_object_or_404, render
-from artists.models import Artist
-from home.spotify import Spotify
-
-
